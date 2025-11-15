e8885609fbd4813b1a5e2b46bc84d4e6e4e9b135
choiboa <musamea99@gmail.com>
2025-11-15 22:00:35 +0900

    💄 Style : 메인/포인트 폰트 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import localFont from "next/font/local"
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,13 +7,29 @@ export const metadata: Metadata = {
   description: "boa's dev blog",
 };
 
+const pretendard = localFont({
+  src: "../public/fonts/pretendard/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  weight: "400 500 600 700 800 900",
+  display: "swap",
+})
+
+const permanentMarker = localFont({
+  src: "../public/fonts/permanentMarker/PermanentMarker-Regular.woff2",
+  variable: "--font-permanent",
+  display: 'swap'
+}) 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html 
+      lang="ko"
+      className={`${pretendard.variable} ${permanentMarker.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
