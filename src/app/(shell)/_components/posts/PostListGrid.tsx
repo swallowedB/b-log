b13347bd33c7583cec0efa5e278740bcd6bbfce5
choiboa <musamea99@gmail.com>
2025-12-16 10:20:26 +0900

    💄 Style : PostSection 퍼블리싱

--- src/app/(shell)/_components/posts/PostListGrid.tsx
+++ src/app/(shell)/_components/posts/PostListGrid.tsx
@@ -1,5 +1,5 @@
-import PostCard from "@/components/common/posts/PostCard";
-import type { PostCardProps } from "@/components/common/posts/PostCard";
+import type { PostCardProps } from "@/app/(shell)/_components/posts/PostCard";
+import PostCard from "@/app/(shell)/_components/posts/PostCard";
 
 interface PostListGridProps {
   posts: PostCardProps[];
@@ -9,19 +9,19 @@ export default function PostListGrid({ posts }: PostListGridProps) {
   return (
     <section
       className="
-        mt-10
+        md:mt-10
         grid gap-8
         grid-cols-1
         sm:grid-cols-2
         lg:grid-cols-4
         items-stretch
+        justify-items-center
       "
     >
       {posts.map((post) => (
-        <PostCard
-          key={post.href}
-          {...post}
-        />
+        <div className="w-full max-w-[400px]" key={post.href}>
+          <PostCard {...post} />
+        </div>
       ))}
     </section>
   );
--- src/app/(shell)/page.tsx
+++ src/app/(shell)/page.tsx
@@ -1,9 +1,20 @@
+import PostSection from "@/app/(shell)/_components/posts/PostSection";
 import Hero from "./_components/home/Hero";
+import { MOCK_POSTS } from "@/app/(shell)/_constants/mockPosts";
 
 export default function HomePage() {
+  const page = 1;
+  const pageSize = 16;
+
   return (
-    <main className="px-5 sm:px-12 md:px-25">
+    <main className="px-5 sm:px-12 lg:px-40 space-y-12 ">
       <Hero />
+      <PostSection
+        posts={MOCK_POSTS}
+        totalCount={MOCK_POSTS.length}
+        page={page}
+        pageSize={pageSize}
+      />
     </main>
   );
 }
